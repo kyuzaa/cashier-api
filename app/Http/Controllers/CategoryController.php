39b06665caaee0ec02172b3b00ff991c5a481045
Kyuza <kevinfebrian12@gmail.com>
2024-10-30 01:22:51 +0700

    Update category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $category = Category::all();
+        $category = Category::with('products')->get();
         return response()->json([
             'status' => true,
             'message' => 'Success Receive Category',
